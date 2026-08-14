89a821f9de16800b56f77a6370004b5082764f42
Yaping Li <leeyp2009@gmail.com>
2026-08-15 01:57:34 +0800

    Update ssheet_sbh.cpp

--- src/pgen/ssheet_sbh.cpp
+++ src/pgen/ssheet_sbh.cpp
@@ -58,6 +58,7 @@ Real Historydvyc(MeshBlock *pmb, int iout);
 Real Historyvxs(MeshBlock *pmb, int iout);
 Real Historydvys(MeshBlock *pmb, int iout);
 
+void GetCylCoord(Coordinates *pco,Real &rad,Real &phi,Real &z,int i,int j,int k);
 void GravitySource(MeshBlock *pmb, const Real time, const Real dt, const AthenaArray<Real> &prim,
      const AthenaArray<Real> &prim_scalar, const AthenaArray<Real> &bcc,
     AthenaArray<Real> &cons, AthenaArray<Real> &cons_scalar);
